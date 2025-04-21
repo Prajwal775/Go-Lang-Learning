e3b979273ac9bc76d711afa9d78deb9104f14344
Prajwal Bharti <prajwalbharti7777@gmail.com>
2025-04-21 18:04:14 +0530

    Post Request In GoLang

--- webreq/main.go
+++ webreq/main.go
@@ -2,39 +2,61 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strings"
 )
 
 func main() {
 	fmt.Println("Welcome to web request handling")
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
-func PerformGetRequest() {
-	const myurl = "http://localhost:3000/get"
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:3000/post"
 
-	response, err := http.Get(myurl)
-	if err != nil {
+
+	//fake json payload
+
+	requestBody := strings.NewReader(`{"course": "golang", "price": 0, "platform": "udemy"}`)
+
+	// http.Post(myurl, "application/json", requestBody)
+	response, err := http.Post(myurl, "application/json", requestBody)
+	if err != nil {	
 		panic(err)
 	}
+	content ,_ :=io.ReadAll(response.Body)
+	fmt.Println("Response from server:", string(content))
 	defer response.Body.Close()
-
 	fmt.Println("Status Code:", response.StatusCode)
-	fmt.Println("Content Length:", response.ContentLength)
 
-	content, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	//One way to print the response
-	// fmt.Println("Response Body:", string(content))
-
-	//Another way to print the response
-	var responseString strings.Builder
-	byteCount, _ := responseString.Write(content)
-	fmt.Println("Number of bytes written:", byteCount)
-	fmt.Println("Response Body:", responseString.String())
 }
+
+// func PerformGetRequest() {
+// 	const myurl = "http://localhost:3000/get"
+	
+
+// 	response, err := http.Get(myurl)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer response.Body.Close()
+
+// 	fmt.Println("Status Code:", response.StatusCode)
+// 	fmt.Println("Content Length:", response.ContentLength)
+
+// 	content, err := io.ReadAll(response.Body)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	//One way to print the response
+// 	// fmt.Println("Response Body:", string(content))
+
+// 	//Another way to print the response
+// 	var responseString strings.Builder
+// 	byteCount, _ := responseString.Write(content)
+// 	fmt.Println("Number of bytes written:", byteCount)
+// 	fmt.Println("Response Body:", responseString.String())
+// }
